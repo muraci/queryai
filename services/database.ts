00e8154abad2512c092129207233c44ecc452887
Murat Açıkgöz <muratacikgoz@live.com>
2023-06-12 20:46:28 +0200

    database build

--- services/database.ts
+++ services/database.ts
@@ -1,8 +1,33 @@
-import { z } from "zod";
+import { CoffeeHelper }from "../islands/CoffeeHelper.tsx";
 
-export const db = await Deno.openKv();
-export const inputSchema = z.array(z.object({
-  id: z.string(),
-  text: z.string().nullable(),
-  completed: z.boolean(),
-}));
+
+import { serve } from "$std/http/server.ts";
+// Open the Deno KV database
+const kv = await Deno.openKv();
+
+serve(async (_req: Request) => {
+  const id = 1;
+
+  const item = {
+    prompt: CoffeeHelper.prompt,
+    result: CoffeeHelper.result,
+    id,
+  };
+
+  const itemKey = ["items", id];
+  // Save the value with the key
+  const _set = await kv.set(itemKey, item);
+
+  console.log(_set); //- { ok: true, versionstamp: "000xxx..." }
+  // Get the value by the key
+  const _get = await kv.get(itemKey);
+
+  console.log(_get);
+  //- {
+  //-   key: [ "items", 1 ],
+  //-   value: { title: "example-1", id: 1 },
+  //-   versionstamp: "000xxx..."
+  //- }
+
+  return new Response(`${JSON.stringify(_get.value)}`);
+});
